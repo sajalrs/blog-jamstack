568bfb2a383955b31d4dc00c5e89b70c73e29fb2
sajalrs <sajalsatyal@gmail.com>
2021-02-03 03:00:28 -0600

    working jamstack starter

---
+++ frontend/src/theme.tsx
@@ -0,0 +1,25 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
+
+// Create a theme instance.
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // main: '#556cd6',
+      main: '#007CAD',
+      light: '#01A1E0',
+      dark: '#004561',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+});
+
+export default theme;
