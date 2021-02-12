4ba919bec12f460d695fa620122373ebca700561
sajalrs <sajalsatyal@gmail.com>
2021-02-11 23:13:33 -0600

    posts/:id pages statically generated from wordpress posts

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ff98f281caad7da8e049";
+/******/ 	var hotCurrentHash = "0d240ea4278157a72580";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
