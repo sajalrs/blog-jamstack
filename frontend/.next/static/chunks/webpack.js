23f3079179a40f75d927632a20a003dbdea1c11e
sajalrs <sajalsatyal@gmail.com>
2021-02-14 13:19:12 -0600

    clicking anywhere on card redirects to article page

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "17471e9c082f702e3d4c";
+/******/ 	var hotCurrentHash = "d57d71e1fe1a476be0ab";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
