98fe0fec24b5e4113b535f7711af3f34a89ec150
sajalrs <sajalsatyal@gmail.com>
2021-02-25 02:29:59 -0600

    todo static site pagination

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0f39bfe8c64fad6edd24";
+/******/ 	var hotCurrentHash = "8a318391d3baa9397569";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
