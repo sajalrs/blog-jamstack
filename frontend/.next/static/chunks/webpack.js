94a8f519ab393773782ef8e84f50808175a9ca74
sajalrs <sajalsatyal@gmail.com>
2021-02-28 13:03:49 -0600

    purged [page].tsx and index page of remnants of pagenav

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "726426f2a72a69e26e54";
+/******/ 	var hotCurrentHash = "801df7d40f313fe92cb2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
