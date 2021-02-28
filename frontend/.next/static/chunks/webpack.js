7d0746c264e678cb07491ccdc8a041fdc49c30d7
sajalrs <sajalsatyal@gmail.com>
2021-02-27 23:30:34 -0600

    navbutton for page disabled if on page

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1448197a04933baf94fe";
+/******/ 	var hotCurrentHash = "db882382f156ca39c562";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
