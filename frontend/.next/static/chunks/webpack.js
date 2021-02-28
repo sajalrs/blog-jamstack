9ef3764b00013fa9bde0cb9a30cabcdcc1e749f7
sajalrs <sajalsatyal@gmail.com>
2021-02-27 23:08:00 -0600

    happy with page number navigation at bottom of list

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1b77a04ae6646307cc71";
+/******/ 	var hotCurrentHash = "1448197a04933baf94fe";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
