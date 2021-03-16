4b03924f043f7efaba1b2d532acb036328fe1a00
sajalrs <sajalsatyal@gmail.com>
2021-03-16 17:43:56 -0500

    seperated frontend and dockerfile

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "44a9a1966a487e16251a";
+/******/ 	var hotCurrentHash = "55bb10cf9a610d5e9c8b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
