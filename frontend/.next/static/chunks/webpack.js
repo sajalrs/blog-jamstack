07232d95aef00f5a4478318f0d91a6a2312c5cf9
sajalrs <sajalsatyal@gmail.com>
2021-03-17 15:28:52 -0500

    fixed project breaking problem by adding phpmyadmin to docker-compose | yarn dev concurrently starts NextJs frontend and docker containers

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "55bb10cf9a610d5e9c8b";
+/******/ 	var hotCurrentHash = "44bcafab1fd01e114722";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
