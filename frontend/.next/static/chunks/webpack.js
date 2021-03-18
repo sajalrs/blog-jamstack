c9ce19cc00e470b759973f9f23c38261ce891fa8
sajalrs <sajalsatyal@gmail.com>
2021-03-18 11:58:04 -0500

    leading and trailing slashes were causing problems in static route generation | fixed issue

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7d268d1cf565e808ee33";
+/******/ 	var hotCurrentHash = "a35f09d55bc99a9c996e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
