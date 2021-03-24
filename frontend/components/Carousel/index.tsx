ce55f751572cc27dc09f7457f8401365758e6089
sajalrs <sajalsatyal@gmail.com>
2021-03-24 16:16:09 -0500

    added sass file so img nolonger cropped on small screens

--- frontend/components/Carousel/index.tsx
+++ frontend/components/Carousel/index.tsx
@@ -2,31 +2,27 @@ import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
 import Typography from "@material-ui/core/Typography";
 import CardContent from "@material-ui/core/CardContent";
-
+import carouselStyles from "./carousel.module.scss";
 type Props = {
-  images: {img: string, caption: string}[];
+  images: { img: string; caption: string }[];
 };
 
 const Carousel = ({ images }: Props) => {
-
-
   const carouselItems = images.map((item) => (
-    
-      <figure className="flex flex-col items-center justify-center max-w-full">
-        <CardContent>
-          <img
-            src={item.img}
-            style={{ maxHeight: "40rem", minHeight: "30rem" }}
-            className="object-cover"
-          />
-        </CardContent>
-        <CardContent>
-          <figcaption className="text-center">
-            <Typography variant="h6">{item.caption}</Typography>
-          </figcaption>
-        </CardContent>
-      </figure>
-
+    <figure className="flex flex-col items-center justify-center max-w-full">
+      <CardContent>
+        <img
+          src={item.img}
+          style={{ maxHeight: "40rem"}}
+          className={`${carouselStyles["img"]} object-cover`}
+        />
+      </CardContent>
+      <CardContent>
+        <figcaption className="text-center">
+          <Typography variant="h6">{item.caption}</Typography>
+        </figcaption>
+      </CardContent>
+    </figure>
   ));
   return (
     <div className="max-w-sm md:max-w-4xl">
